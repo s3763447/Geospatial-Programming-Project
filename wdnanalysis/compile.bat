5417873d35bf018e5e214e160eceae70c8f15a2c
s3763447 <80402752+s3763447@users.noreply.github.com>
2021-10-13 13:02:40 +0300

    Delete wdnanalysis directory

--- wdnanalysis/compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\QGIS 3.20.1\bin\o4w_env.bat"
-call "C:\Program Files\QGIS 3.20.1\etc\preremove\python3-pyqt5.bat"
-call "C:\Program Files\QGIS 3.20.1\etc\ini\python3.bat"
-
-@echo on
-pyrcc5 -o resources.py resources.qrc
